322ce8812d7b39bca2958c28c8f2b53895ddaebb
KevinHung <kevinaugust145@gmail.com>
2024-07-14 14:51:23 +0800

    add page control

--- page control/ViewController.swift
+++ page control/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var currentPage=0
+    @IBOutlet weak var pageControl: UIPageControl!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updatePageControl()
     }
 
-
+    @IBAction func left(_ sender: Any) {
+        if currentPage>0
+        {
+            currentPage-=1
+            updatePageControl()
+        }
+    }
+    @IBAction func right(_ sender: Any) {
+        if currentPage < pageControl.numberOfPages
+        {
+            currentPage+=1
+            pageControl.currentPage=currentPage
+            updatePageControl()
+        }
+    }
+    func updatePageControl()
+    {
+        pageControl.currentPage=currentPage
+    }
 }
 
