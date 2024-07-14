b036f0921365c043aef3cc7e5e1a3ecad18b839d
KevinHung <kevinaugust145@gmail.com>
2024-07-14 12:40:40 +0800

    Initial Commit

---
+++ page control/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  page control
+//
+//  Created by 洪宗燦 on 2024/7/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ page controlTests/page_controlTests.swift
@@ -0,0 +1,17 @@
+//
+//  page_controlTests.swift
+//  page controlTests
+//
+//  Created by 洪宗燦 on 2024/7/14.
+//
+
+import Testing
+@testable import page_control
+
+struct page_controlTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
